8904b4c3182a0af9ddcedbdbe0ca890c90b2cbb2
Shivam Misra <shivammisra@ucsb.edu>
2020-01-18 23:44:25 -0800

    Updates

---
+++ precache-manifest.b183b87db320b5688c6d7d7946540e66.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76f5028dfafc459c6968683470723a6d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d27eb6d6364ed0837248",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "ef9804a867f77f9e15ac",
+    "url": "/static/js/2.ec1606be.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.ec1606be.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d27eb6d6364ed0837248",
+    "url": "/static/js/main.67f7d123.chunk.js"
+  },
+  {
+    "revision": "6e7d85ad969107dd34f7",
+    "url": "/static/js/runtime-main.54edee9e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
